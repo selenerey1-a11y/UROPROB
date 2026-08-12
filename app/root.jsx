806db2f15a0ecb5e5835f93c0f542a8b35888d9f
selenerey1-a11y <selenerey1@gmail.com>
2026-08-12 13:00:17 +0200

    Update root.jsx

--- app/root.jsx
+++ app/root.jsx
@@ -149,6 +149,22 @@ export function Layout({children}) {
   return (
     <html lang="en">
       <head>
+        <script
+  dangerouslySetInnerHTML={{
+    __html: `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window,document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1586370229709460');
+      fbq('track', 'PageView');
+    `,
+  }}
+/>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="stylesheet" href={tailwindCss}></link>
