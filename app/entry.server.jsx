68e787d51489f5b0a05de20ed9bde715afe55793
selenerey1-a11y <selenerey1@gmail.com>
2026-08-13 08:34:13 +0200

    Update Content Security Policy sources for scripts and images

--- app/entry.server.jsx
+++ app/entry.server.jsx
@@ -22,9 +22,9 @@ export default async function handleRequest(
     checkoutDomain: context.env.PUBLIC_CHECKOUT_DOMAIN,
     storeDomain: context.env.PUBLIC_STORE_DOMAIN,
   },
-  scriptSrc: ["'self'", 'https://connect.facebook.net'],
-  connectSrc: ["'self'", 'https://connect.facebook.net', 'https://www.facebook.com'],
-  imgSrc: ["'self'", 'https://www.facebook.com'],
+ scriptSrc: ["'self'", 'https://cdn.shopify.com', 'https://shopify.com', 'https://connect.facebook.net'],
+connectSrc: ["'self'", 'https://cdn.shopify.com', 'https://shopify.com', 'https://connect.facebook.net', 'https://www.facebook.com'],
+imgSrc: ["'self'", 'https://cdn.shopify.com', 'https://shopify.com', 'https://www.facebook.com'],
 });
 
   const body = await renderToReadableStream(
